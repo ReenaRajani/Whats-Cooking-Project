9ad2b66990e92cce84efbf4cbad923dbf34fc066
ReenaRajani <reenahrajani@gmail.com>
2015-06-02 12:41:20 +1000

    Search works, basic MVP version

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,24 +3,48 @@
 # app_key = "5cc84166d69454f54ed43fb1bcb9b858
 
 class RecipesController < ApplicationController
-  def index
-    app_id = "c5975da7"
-    app_key = "5cc84166d69454f54ed43fb1bcb9b858"
-    @title =params[:title]
-    if @title 
-      url = "https://api.edamam.com/search?q=#{ @title }&app_id=#{ app_id }&app_key=#{ app_key }"
+  
+  # set the api.edamam.com's appkey and appid
+  def initialize
+    @app_id = "c5975da7"
+    @app_key = "5cc84166d69454f54ed43fb1bcb9b858"
+  end
+
+
+  #  Search based on the recipe name 
+  # gets the recipename as input and then calls the search_results function
+  def search_recipes
+    @title = params[:recipe_name]
+    search_results(@title)
+  end
+
+# search based on the ingredient(s)
+# title will be a csv of ingredients got as an input from the user 
+  def multiple_ingredients_search
+    @title = params[:ingredient1],params[:ingredient2],params[:ingredient3]
+    search_results(@title)
+    # binding.pry
+  end
+
+
+#Search the results based on the arg title, which is either the recipe name or the listof ingredients
+# This is where we display the results
+#sets the url and the HTTParty generates the url . results are fed in to the recipelist and the search results page is displayed
+  def search_results( title = @title )
+    if title 
+      url = "https://api.edamam.com/search?q=#{ title }&app_id=#{ @app_id }&app_key=#{ @app_key }"
       @recipe_data = HTTParty.get url if url
       @recipe_list = []
       @recipe_data['hits'].each do |recipe|
         @recipe_list << recipe['recipe']
       end
+      render "recipes/search_results"
     end
- 
   end
   
   def home
-    
-
+  end
+  def index
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 root :to => 'recipes#home'
-get '/recipes' => 'recipes#index'
- 
+get '/recipes/search' => 'recipes#search_recipes'
+get '/recipes/search_ingredients' => 'recipes#multiple_ingredients_search'
+get '/recipes/search_results' => 'recipes#search_results', :as => "search_results"
+
 end
